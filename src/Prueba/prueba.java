806be903ff7ee3ed3b08e051ba548a86854eb6f6
L3CR0NN <danigfernandez1@gmail.com>
2025-12-05 14:30:10 +0100

    Version del administrador, main con ToDo listo //05//12//2025

---
+++ src/Prueba/prueba.java
@@ -0,0 +1,25 @@
+package Prueba;
+
+public class prueba {
+	public int pedirNumero() { //TODO Jorge se encarga de esta funcion
+		
+	}
+	
+	public int sumarNumero() { //TODO Jhonatan se encarga de esta funcion
+		
+	}
+	
+	public int multiplicarNumero() { //TODO Hugo se encarga de esta funcion
+		
+	}
+	
+	public static void main(String[] args) {
+		int num1, num2;
+		
+		//Llamar a pedirNumero
+		//Llamar a sumarNumero
+		//Llamar a multipplicarNumero
+
+	}
+
+}
