c70f3a548cd65c5c86cfa6cd8569674470402694
hcaroli381 <hcaroli381@g.educaand.es>
2025-12-09 10:06:25 +0100

    Añado la funcionalidad de multiplicacion

--- src/Prueba/prueba.java
+++ src/Prueba/prueba.java
@@ -1,24 +1,28 @@
 package Prueba;
 
 public class prueba {
-	public int pedirNumero() { //TODO Jorge se encarga de esta funcion
-		
+	public int pedirNumero() { // TODO Jorge se encarga de esta funcion
+
 	}
-	
-	public int sumarNumero() { //TODO Jhonatan se encarga de esta funcion
-		
+
+	public int sumarNumero() { // TODO Jhonatan se encarga de esta funcion
+
 	}
-	
-	public int multiplicarNumero() { //TODO Hugo se encarga de esta funcion
-		
+
+	public int multiplicarNumero(int num1, int num2) { // TODO Hugo se encarga de esta funcion
+		int multiplicacion;
+
+		multiplicacion = num1 * num2;
+
+		return multiplicacion;
 	}
-	
+
 	public static void main(String[] args) {
 		int num1, num2;
-		
-		//Llamar a pedirNumero
-		//Llamar a sumarNumero
-		//Llamar a multipplicarNumero
+
+		// Llamar a pedirNumero
+		// Llamar a sumarNumero
+		// Llamar a multipplicarNumero
 
 	}
 
